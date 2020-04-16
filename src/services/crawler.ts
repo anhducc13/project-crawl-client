879813ed3637d9de0d9691daa004c82bcfb5ec24
anhducc13 <trantienduc10@gmail.com>
2020-04-16 16:14:11 +0700

    add env

--- src/services/crawler.ts
+++ src/services/crawler.ts
@@ -1,20 +1,7 @@
 import axios from 'axios';
 
-// axios.interceptors.request.use(config => {
-//     return config;
-// }, error => {
-//     return Promise.reject(error);
-// });
-//
-// axios.interceptors.response.use(response => {
-//     response.headers['Access-Control-Allow-Origin'] = '*';
-//     response.headers['Content-Type'] = 'text/html';
-//     return response;
-// }, error => {
-//     console.log(error);
-//     return Promise.reject(error);
-// });
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const getProduct = (url: string) => {
-    return axios.get("https://crawl-server.herokuapp.com/api/crawl-detail/product", { params: { url } }).then(res => res.data);
+    return axios.get(`${API_URL}/api/crawl-detail/product`, { params: { url } }).then(res => res.data);
 };
